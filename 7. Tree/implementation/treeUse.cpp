9d96d4a3a17657f37b6da41b809354552b473092
Jainam Panchal <66761580+jainam-panchal@users.noreply.github.com>
2023-05-19 10:34:24 +0530

    Tree Class

---
+++ 7. Tree/implementation/treeUse.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "tree.h"
+using namespace std;
+
+void printTree(TreeNode<int>* root) {
+    // edge case, not base case
+    if(root == NULL){
+        cout << "Empty Root" << endl;
+        return;
+    }
+    cout << root->data << " : ";
+    for(int i=0; i<root->children.size(); i++){
+        cout << root->children[i]->data << ",";
+    }
+    cout << endl;
+        for(int i=0; i<root->children.size(); i++){
+        printTree(root->children[i]);
+    }
+}
+
+TreeNode<int>* takeInput() {
+    cout << "Enter data : " ;
+    int value;
+    cin >> value;
+    TreeNode<int>* root = new TreeNode<int>(value);
+
+    int totalChild;
+    cout << "How many children " << root->data << " have? : ";
+    cin >> totalChild;
+
+    for(int i=0; i<totalChild; i++){
+        TreeNode<int>* temp = takeInput();
+        root->children.push_back(temp);
+    }
+    
+    return root;
+}
+
+int main() {
+    // TreeNode<int>* root = new TreeNode<int>(99);
+    // TreeNode<int>* node1 = new TreeNode<int>(1);
+    // TreeNode<int>* node2 = new TreeNode<int>(2);
+    // TreeNode<int>* node3 = new TreeNode<int>(3);
+    // root->children.push_back(node1);
+    // root->children.push_back(node2);
+    // node1->children.push_back(node3);
+    // printTree(root);    
+
+    TreeNode<int>* root2 = takeInput();
+    printTree(root2);
+
+}
